8995de15123f9c4f3ed818caa4927ecd97d1b8dc
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2025-08-16 20:35:27 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -6,10 +6,18 @@ def startGame():
     if path == "out":
         print("Congrats, you didn't even start the adventure but you live! :D")
     else:
-        scarySoundPath = input("You hear a scary sound from the right side, which way you go?(right/left)").lower()
+        scarySoundPath = input("You hear a scary sound from the right side, which way you go?(right/left): ").lower()
         if scarySoundPath == "left":
-            possibleEnd = input("Now you see the exit, do you want to leave or stay?(stay/leave):").lower()
+            possibleEnd = input("Now you see the exit, do you want to leave or stay?(stay/leave): ").lower()
             if possibleEnd == "leave":
                 print("Congrats, you didn't die!")
             else:
                 print("A bear attacked you and you died! =(")
+        else:
+            print("The sound you heard is from a vampire!!!!!!! OH NOOOOOOOO, YOU DIED HAHAHAHAHAH")
+
+while True:
+    startGame()
+    playAgain = input("Do you want to play again?(Y/N): ").lower()
+    if playAgain == "n":
+        break
