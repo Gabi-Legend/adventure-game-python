9cd99d22bd026a60b76501bcb40dcf9b0bf22c7e
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2025-08-16 20:32:20 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,15 @@
+print("Welcome to adventure game!")
+
+path = input("You are right next to a creepy forest, will you go in or stay out?(in/out): ").lower()
+
+def startGame():
+    if path == "out":
+        print("Congrats, you didn't even start the adventure but you live! :D")
+    else:
+        scarySoundPath = input("You hear a scary sound from the right side, which way you go?(right/left)").lower()
+        if scarySoundPath == "left":
+            possibleEnd = input("Now you see the exit, do you want to leave or stay?(stay/leave):").lower()
+            if possibleEnd == "leave":
+                print("Congrats, you didn't die!")
+            else:
+                print("A bear attacked you and you died! =(")
